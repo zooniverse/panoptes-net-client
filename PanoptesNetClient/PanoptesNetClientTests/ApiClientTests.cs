68c6ec373099ba7c22b91888b00d26074afc52ba
Will Granger <twgranger@gmail.com>
2018-07-03 13:16:31 -0500

    Write Valid Tests that include Async Method

--- PanoptesNetClient/PanoptesNetClientTests/ApiClientTests.cs
+++ PanoptesNetClient/PanoptesNetClientTests/ApiClientTests.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Moq;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using PanoptesNetClient;
+using PanoptesNetClientTests;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PanoptesNetClientTests_NUnit
@@ -8,13 +11,15 @@ namespace PanoptesNetClientTests_NUnit
     [TestFixture]
     public class ApiClientTests
     {
-        // <summary>
-        // Call the initial instance to set up and create the client singleton
-        // </summary>
-        [Test]
-        public void CanInit()
+        private Mock<FakeHttpMessageHandler> _fakeHttpMessageHandler;
+        private HttpClient _httpClient;
+        public ApiClient Client;
+
+        [SetUp]
+        public void SetUp()
         {
-            ApiClient client = new ApiClient();
+            _fakeHttpMessageHandler = new Mock<FakeHttpMessageHandler> { CallBase = true };
+            _httpClient = new HttpClient(_fakeHttpMessageHandler.Object);
         }
 
         // <summary>
@@ -23,18 +28,28 @@ namespace PanoptesNetClientTests_NUnit
         [Test]
         public void Type()
         {
-            ApiClient client = new ApiClient();
-            IRequest request = client.Type("projects");
+            IRequest request = Client.Type("projects");
             string expected = "api/projects/";
 
             Assert.AreEqual(expected, request.Endpoint);
         }
 
+        // <summary>
+        // Test that GetAsync returns the correct object that is not null
+        // </summary>
         [Test]
         public async Task GetAsync()
         {
-            ApiClient client = new ApiClient();
-            JObject resource = await client.Type("projects").ById("45").GetAsync();
+            _fakeHttpMessageHandler.Setup(f => f.Send(It.IsAny<HttpRequestMessage>())).Returns(new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Content = new StringContent("{\"success\": false,\"error-codes\": [\"It's a fake error!\",\"It's a fake error\"]}")
+            });
+            Client = new ApiClient(_httpClient);
+
+            var result = await Client.Type("projects").ById("45").GetAsync();
+            Assert.That(result, Is.Not.Null);
+            Assert.IsInstanceOf<JObject>(result);
         }
     }
 }
